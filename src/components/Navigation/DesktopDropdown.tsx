e83e42820633aca8643455fa53599dec66eefd9e
NatnaelDemelash <natidemelash074@gmail.com>
2025-04-15 21:56:16 +0300

    create a navigation bar

--- src/components/Navigation/DesktopDropdown.tsx
+++ src/components/Navigation/DesktopDropdown.tsx
@@ -1,13 +1,9 @@
-import { NavigationLink } from "../../utils/contentTypes"
+import { NavigationLink } from "../../utils/contentTypes";
 
 interface DesktopDropdownProps {
-  links?: NavigationLink[]; 
+  links?: NavigationLink[];
 }
 
-export default function DesktopDropdown({links} : DesktopDropdownProps) {
-  return (
-    <div>
-      DesktopDropdowns
-    </div>
-  )
+export default function DesktopDropdown({ links }: DesktopDropdownProps) {
+  return <div>dropdown</div>;
 }
